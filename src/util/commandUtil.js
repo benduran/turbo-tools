646371ddef1c4989ffae5ab5b82b4b941c08209b
benduran <stratodyne@gmail.com>
2023-07-21 19:53:32 -0700

    chore: fixed grammar and bumped lets-version dep

--- src/util/commandUtil.js
+++ src/util/commandUtil.js
@@ -38,7 +38,7 @@ export function getVersionAndPublishBaseYargs(yargs) {
     .option('noCommit', {
       default: false,
       description:
-        'If true, will modify all required files but leave them uncommitted after all operations as completed. This will also prevent a git push from occurring',
+        'If true, will modify all required files but leave them uncommitted after all operations have completed. This will also prevent a git push from occurring',
       type: 'boolean',
     })
     .option('noFetchAll', {
